9e511f36a6dd87490cb09badaa76c0f70c8fd7fa
ijbd <ijbd@umich.edu>
2021-05-03 12:27:05 -0600

    Add technology tag.

--- RenewableSites.py
+++ RenewableSites.py
@@ -9,7 +9,7 @@ import numpy as np
 import pandas as pd
 import os
 
-def _getAnnualCF(filename, cf_only=False):
+def _getAnnualCF(filename, cf_only):
     re = pd.read_csv(filename,index_col=0)
 
     # get locations
@@ -43,9 +43,7 @@ def getAnnualCF(solar_filename, wind_filename, cf_only=False):
     if not cf_only:
         wind['Technology'] = 'w'
 
-    renewableSites = solar.append(wind)
-
-    return renewableSites
+    return solar.append(wind)
 
 def _getHourlyCF(filename, cf_only):
     re = pd.read_csv(filename,index_col=0)
@@ -55,8 +53,9 @@ def _getHourlyCF(filename, cf_only):
     lons = [float(c.split()[1]) for c in re.columns]
     
     cf = pd.DataFrame()
-    cf['Latitude'] = lats
-    cf['Longitude'] = lons
+    if not cf_only:
+        cf['Latitude'] = lats
+        cf['Longitude'] = lons
     gen = re.values.T
 
     for i in range(gen.shape[1]):
@@ -75,29 +74,12 @@ def getHourlyCF(solar_filename, wind_filename, cf_only=False):
     -------
         `renewableSites` (pd.Series) : Series of lat/lons
     '''
-    solar = pd.read_csv(solar_filename,index_col=0)
-    wind = pd.read_csv(wind_filename,index_col=0)
-
-    # get locations
-    solarLats = [float(c.split()[0]) for c in solar.columns]
-    solarLons = [float(c.split()[1]) for c in solar.columns]
-    windLats = [float(c.split()[0]) for c in wind.columns]
-    windLons = [float(c.split()[1]) for c in wind.columns]
-
-    # fill
-    renewableSites = pd.DataFrame()
+    solar = _getHourlyCF(solar_filename,cf_only)
     if not cf_only:
-        renewableSites['Latitude'] = np.append(solarLats,windLats)
-        renewableSites['Longitude'] = np.append(solarLons,windLons)
+        solar['Technology'] = 's'
 
-    # get generation 
-    solarGen = solar.values.T
-    windGen = wind.values.T
+    wind = _getHourlyCF(wind_filename,cf_only)
+    if not cf_only:
+        wind['Technology'] = 'w'
 
-    gen = np.concatenate((solarGen,windGen))
-
-    # Annual CF
-    for i in range(gen.shape[1]):
-        renewableSites['Hour {} CF'.format(i)] = gen[:,i]
-
-    return renewableSites
+    return solar.append(wind)
